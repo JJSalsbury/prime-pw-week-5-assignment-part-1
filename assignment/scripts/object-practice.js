7620ff21389e7ec9610cee35b42f9c4a513f34c4
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-14 13:55:21 -0600

    Object-Practice.js: Draft 1 - 5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,37 +9,58 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
-  // TODO - add properties here
-};
+  // TODO - add properties hereLet literalMe = {
+  firstName: 'JJ',
+  lastName: 'Salsbury',
+  hasSiblings: true,
+  shoeCount: 20,
+  favThreeFoods: ['pizza', 'pasta', 'subs']
+};//end
+
 console.log('A little about me:', me);
 
 /* 2. Accessing object properties.
-  - Create a variable called fullName 
-  - Use the firstName & lastName properties of the object you 
-    created above to set its value. 
+  - Create a variable called fullName
+  - Use the firstName & lastName properties of the object you
+    created above to set its value.
   - Console.log fullName
 */
+let fullname = me.firstName + ' ' + me.lastName;
 
+console.log( fullname );
 
 
 /* 3. Nested arrays
   - Console.log your first favorite food
-  - Console.log your last favorite food 
+  - Console.log your last favorite food
 */
-
-
+console.log( me.favThreeFoods[0]);
+console.log( me.favThreeFoods[2]);
 
 /* 4. Change a property of an existing object.
   - Console.log your current shoe count.
   - Guess what?! You just got a new pair of shoes!!!
   - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
+  - Console.log your updated shoe count.
 */
-
-
+console.log( me.shoeCount );
+// got new pair of shoes
+function addShoes( pairofshoes ) {
+  me.shoeCount = me.shoeCount + pairofshoes;
+  return me.shoeCount;
+} //end add Shoes
+addShoes(1);
+console.log( 'New shoe count:', me.shoeCount );
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
   - Do this the same way you updated the variable above
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+console.log( me );
+function addProperty( property ) {
+  me = me + property;
+  return me;
+} //end
+addProperty( 'favoriteColor' );
+console.log( me );
